2d2f6810394cb0c1fc967d6f00c87fcf19a050a8
f1nesse13 <philly.teg@gmail.com>
2022-01-14 20:38:04 -0500

    directory changes

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-import chalk from "chalk"
-import fs from "fs"
-import prompts from "prompts"
+import main from "./src/main.js"
 
-const main = async () => {
-	const response = await multipleChoice()
-	console.log("Final response", response)
-	return
-}
-
-const multipleChoice = async () => {
-	const response = await prompts([
-		{
-			type: "text",
-			name: "name",
-			message: "Name",
-		},
-		{
-			type: "select",
-			name: "color",
-			message: (prev) => (prev === "" ? "Pick colors" : "sroloc kciP"),
-			choices: (p, v) =>
-				p == "Joe"
-					? [{ title: "color test", value: "eoJ" }]
-					: [
-							{ title: "Red", value: "#ff0000" },
-							{ title: "Green", value: "#00ff00" },
-							{ title: "Blue", value: "#0000ff" },
-					  ],
-		},
-	])
-
-	return response
-}
-
-main()
+await main()
